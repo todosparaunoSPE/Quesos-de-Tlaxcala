3efb8be1a829c58a55f7412e324769e60b674a8d
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-07-02 10:41:04 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  2 10:27:28 2024
+
+@author: jperezr
+"""
+
+import streamlit as st
+
+def main():
+    st.title('Quesos de Tlaxcala, Tlax.')
+    st.subheader('Precios')
+
+    precios = {
+        "Queso Oaxaca 1K": 150,
+        "Queso Oaxaca 1/2 k": 80,
+        "Queso Panela 1K": 150,
+        "Queso Panela 1/2 K": 80,
+        "Queso Botanero 1K": 200,
+        "Queso Botanero 1/2 k": 100,
+        "Requeson 1kg": 100,
+        "Requeson 1/2 K": 60,
+        "Queso Fresco redondo 3 piezas": 120,
+        "Tlacoyos de requeson con chicharron 1 docena": 100,
+        "Tlacoyos solo requeson 1 docena": 100
+    }
+
+    for producto, precio in precios.items():
+        st.header(f"{producto}: ${precio}")
+
+    st.sidebar.title("Informes")
+    st.sidebar.write("Javier Horacio Pérez Ricárdez")
+    st.sidebar.write("Móvil: +52 55 7425 5593")
+    st.sidebar.markdown("---")
+    st.sidebar.write("© 2024 Quesos de Tlaxcala, Tlax. Todos los derechos reservados.")
+
+if __name__ == "__main__":
+    main()
